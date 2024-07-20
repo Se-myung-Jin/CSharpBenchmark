aac22d75b320b7101f055e5daecacd890a3d1748
Se-myung-Jin <tpaud94@naver.com>
2024-07-20 23:47:00 +0900

    compare toString and boxing

---
+++ CSharpBenchmark/CompareToString.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmark
+{
+    [MemoryDiagnoser]
+    public class CompareToString
+    {
+        [Benchmark]
+        public void Boxing()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var s = string.Format("Test: {0}", i);
+            }
+        }
+
+
+        [Benchmark]
+        public void CastToString()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var s = string.Format("Test: {0}", i.ToString());
+            }
+        }
+    }
+}
