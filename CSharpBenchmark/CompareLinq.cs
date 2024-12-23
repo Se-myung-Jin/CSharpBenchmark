599649eb1fd7176c774bd419092c1efbef362bcc
Se-myung-Jin <tpaud94@naver.com>
2024-12-24 00:02:20 +0900

    Compare Linq 추가

---
+++ CSharpBenchmark/CompareLinq.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBenchmark
+{
+    [MemoryDiagnoser]
+    public class CompareLinq
+    {
+        public int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        [Benchmark]
+        public void Count()
+        {
+            var list = arr.Select(x => x).Where(x => x > 4).ToList();
+        }
+
+
+        [Benchmark]
+        public void Any()
+        {
+            var list = from n in arr
+                       where n > 4
+                       select n;
+        }
+
+        [Benchmark]
+        public void Any2()
+        {
+            var list = from n in arr
+                       where n > 4
+                       select n;
+        }
+    }
+}
--- CSharpBenchmark/CompareToString.cs
+++ CSharpBenchmark/CompareToString.cs
@@ -10,7 +10,7 @@ namespace CSharpBenchmark
         {
             for (var i = 0; i < 100; i++)
             {
-                var s = string.Format("Test: {0}", i);
+                var s =($"Test: {0}", i);
             }
         }
 
