24ad4807ad77f805d9b53ec0cbc5548d26a26311
Semyung Jin <tpaud94@naver.com>
2025-02-19 10:57:23 +0900

    StringBuilder 스트링 추가 테스트 성능 추가

---
+++ CSharpBenchmark/CompareToStringBuilder.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace CSharpBenchmark
+{
+    [MemoryDiagnoser]
+    public class CompareToStringBuilder
+    {
+        [Benchmark(Baseline = true)]
+        public void StringForLoop()
+        {
+            string ret = string.Empty;
+            for (var i = 0; i < 100; i++)
+            {
+                ret += i.ToString();
+            }
+            Console.WriteLine(ret);
+        }
+
+
+        [Benchmark]
+        public void StringBuilderForLoop()
+        {
+            var str = new StringBuilder();
+            for (var i = 0; i < 100; i++)
+            {
+                str.Append(i.ToString());
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
