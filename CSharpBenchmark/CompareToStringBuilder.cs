1dee1ddb48787a6431c33e7b039c3e3de3e94c29
Semyung Jin <tpaud94@naver.com>
2025-02-21 11:53:03 +0900

    기본값 복원

--- CSharpBenchmark/CompareToStringBuilder.cs
+++ CSharpBenchmark/CompareToStringBuilder.cs
@@ -10,7 +10,7 @@ namespace CSharpBenchmark
         public void StringForLoop()
         {
             string ret = string.Empty;
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 100000000; i++)
             {
                 ret += i.ToString();
             }
@@ -22,7 +22,7 @@ namespace CSharpBenchmark
         public void StringBuilderForLoop()
         {
             var str = new StringBuilder();
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 100000000; i++)
             {
                 str.Append(i.ToString());
             }
